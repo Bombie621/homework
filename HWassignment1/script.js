08068f4fc32cf030ba6d8ac59537374b200f7c09
Bombie621 <84678541+Bombie621@users.noreply.github.com>
2021-05-23 23:14:58 +0300

    Delete script.js

--- HWassignment1/script.js
+++
@@ -1,7 +0,0 @@
-function imgSlider(anything){
-    document.querySelector('.starbucks1').src = anything;
-}
-function changeCircleColor(color){
-        const circle = document.querySelector('.circle');
-        circle.style.background = color;
-}
